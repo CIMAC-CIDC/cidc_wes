320f90fcadaf5deea28661e29a35513b6f811f66
Aashna Jhaveri <aashna@jimmy.harvard.edu>
2019-02-20 22:38:22 +0000

    facets_plots.R -changed to command line arguments

--- modules/scripts/facets_plots.R
+++ modules/scripts/facets_plots.R
@@ -1,42 +1,32 @@
 #!/usr/bin/env Rscript
-setwd("/usr/gitc/pileup") ##set this to  the actual  pileup path-ask len 
 library(pctGCdata)
 library(facets)
 
-getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
-
+facets_plots<-function(arg_in, arg_out, arg_name) {
+rcmat = readSnpMatrix(arg_in, perl.pileup = F)
 filenames <- c()
 modes <- c()
 puritys <- c()
 ploidys <- c()
 dipLogRs <- c()
 
-for(file in list.files(pattern='.gz$')) {
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
 
-  purity.vals <- c()
-  ploidy.vals <- c()
-  dipLogR.vals <- c()
-  cncf.vals <- list()
-  rcmat = readSnpMatrix(file, perl.pileup = F)
+for(i in seq(10)) {
+  xx = preProcSample(rcmat)
+  oo=procSample(xx, cval=150)
+  fit=emcncf(oo, maxiter = 1000)
+  purity.vals[i] <- signif(fit[['purity']], 3)
+  ploidy.vals[i] <- fit[['ploidy']]
+  dipLogR.vals[i] <- oo[['dipLogR']]
+  cncf.vals[[i]] <- fit[['cncf']]
+  plotSample(x=oo, emfit=fit)
+}
 
-  for(i in seq(10)) {
-    xx = preProcSample(rcmat)
-    oo=procSample(xx, cval=150)
-    fit=emcncf(oo, maxiter = 1000)
-    purity.vals[i] <- signif(fit[['purity']], 3)
-    ploidy.vals[i] <- fit[['ploidy']]
-    dipLogR.vals[i] <- oo[['dipLogR']]
-    cncf.vals[[i]] <- fit[['cncf']]
-    plotSample(x=oo, emfit=fit) # plot
-  }
-
-  print(file)
-  print(purity.vals)
-
-  mode <- getmode(purity.vals)
+mode <- getmode(purity.vals)
   index <- which(purity.vals == mode)[1]
   purity <- purity.vals[index]
   ploidy <- ploidy.vals[index]
@@ -48,9 +38,17 @@ for(file in list.files(pattern='.gz$')) {
   puritys <- c(puritys, purity)
   ploidys <- c(ploidys, ploidy)
   dipLogRs <- c(dipLogRs, dipLogR)
-}
+
 
 df <- data.frame(name=filenames, mode=modes, purity=puritys, ploidy=ploidys, dipLogR=dipLogRs)
 df.iter <- data.frame(name=filenames, purity=purity.vals, ploidy=ploidy.vals, dipLogR=dipLogR.vals)
 write.table(df, "Facets_output.txt", quote=F, sep="\t", row.names=F)
 write.table(df.iter, "Facets_iterations.txt", quote=F, sep="\t", row.names=F)
+}
+
+args <- commandArgs( trailingOnly = TRUE )
+arg_in = args[1]
+arg_out = args[2]
+arg_name = args[3]
+
+facets_plots(arg_in,arg_out,arg_name)
