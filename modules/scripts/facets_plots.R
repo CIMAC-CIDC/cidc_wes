dfe60bc3ed3a037f53dac3d5a3879967491e44a8
Aashna Jhaveri <aashna@jimmy.harvard.edu>
2019-02-21 00:43:20 +0000

    facets_plots.R - code runs on cmd line

--- modules/scripts/facets_plots.R
+++ modules/scripts/facets_plots.R
@@ -1,9 +1,11 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/env Rscript                                                                                                                                                                       #!/usr/bin/env Rscript
 library(pctGCdata)
 library(facets)
 
-facets_plots<-function(arg_in, arg_out, arg_name) {
+facets_plots<-function(arg_in,arg_out, arg_name) {
 rcmat = readSnpMatrix(arg_in, perl.pileup = F)
+#print(rcmat)
+#stop()
 filenames <- c()
 modes <- c()
 puritys <- c()
@@ -15,40 +17,50 @@ getmode <- function(v) {
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-for(i in seq(10)) {
-  xx = preProcSample(rcmat)
-  oo=procSample(xx, cval=150)
-  fit=emcncf(oo, maxiter = 1000)
-  purity.vals[i] <- signif(fit[['purity']], 3)
-  ploidy.vals[i] <- fit[['ploidy']]
-  dipLogR.vals[i] <- oo[['dipLogR']]
-  cncf.vals[[i]] <- fit[['cncf']]
-  plotSample(x=oo, emfit=fit)
-}
+#for(file in list.files(pattern='.gz$')) {
 
-mode <- getmode(purity.vals)
-  index <- which(purity.vals == mode)[1]
-  purity <- purity.vals[index]
-  ploidy <- ploidy.vals[index]
-  dipLogR <- dipLogR.vals[index]
-  write.table(data.frame(cncf.vals[[index]]), paste0(file,'.cncf'), quote=F, sep="\t", row.names=F)
+  purity.vals <- c()
+  ploidy.vals <- c()
+  dipLogR.vals <- c()
+  cncf.vals <- list()
+ # rcmat = readSnpMatrix(file, perl.pileup = F)
 
-  filenames <- c(filenames, file)
-  modes <- c(modes, mode)
-  puritys <- c(puritys, purity)
-  ploidys <- c(ploidys, ploidy)
-  dipLogRs <- c(dipLogRs, dipLogR)
+  for(i in seq(10)) {
+        xx = preProcSample(rcmat)
+        oo=procSample(xx, cval=150)
+        fit=emcncf(oo, maxiter = 1000)
+        purity.vals[i] <- signif(fit[['purity']], 3)
+        ploidy.vals[i] <- fit[['ploidy']]
+        dipLogR.vals[i] <- oo[['dipLogR']]
+        cncf.vals[[i]] <- fit[['cncf']]
+        plotSample(x=oo, emfit=fit)
+   }
 
+   mode <- getmode(purity.vals)
+   index <- which(purity.vals == mode)[1]
+   purity <- purity.vals[index]
+   ploidy <- ploidy.vals[index]
+   dipLogR <- dipLogR.vals[index]
+   write.table(data.frame(cncf.vals[[index]]), paste0(arg_out,arg_name,'.cncf'), quote=F, sep="\t", row.names=F)
+
+   filenames <- c(filenames, arg_in)
+   modes <- c(modes, mode)
+   puritys <- c(puritys, purity)
+   ploidys <- c(ploidys, ploidy)
+   dipLogRs <- c(dipLogRs, dipLogR)
+#}
 
 df <- data.frame(name=filenames, mode=modes, purity=puritys, ploidy=ploidys, dipLogR=dipLogRs)
 df.iter <- data.frame(name=filenames, purity=purity.vals, ploidy=ploidy.vals, dipLogR=dipLogR.vals)
-write.table(df, "Facets_output.txt", quote=F, sep="\t", row.names=F)
-write.table(df.iter, "Facets_iterations.txt", quote=F, sep="\t", row.names=F)
+write.table(df,paste0(arg_out,arg_name,'.purity.txt'), quote=F, sep="\t", row.names=F)
+write.table(df.iter, paste0(arg_out,arg_name,'.purityfinal.txt') , quote=F, sep="\t", row.names=F)
 }
 
 args <- commandArgs( trailingOnly = TRUE )
-arg_in = args[1]
+arg_in= args[1]
 arg_out = args[2]
 arg_name = args[3]
 
 facets_plots(arg_in,arg_out,arg_name)
+
+
