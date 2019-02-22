eac6124419d199933c8c9d88220450ff227852b2
Aashna Jhaveri <aashna@jimmy.harvard.edu>
2019-02-22 23:10:15 +0000

    facets_plots.R -added the file names

--- modules/scripts/facets_plots.R
+++ modules/scripts/facets_plots.R
@@ -3,37 +3,34 @@ library(pctGCdata)
 library(facets)
 
 facets_plots<-function(arg_in,arg_out, arg_name) {
-rcmat = readSnpMatrix(arg_in, perl.pileup = F)
-#print(rcmat)
-#stop()
-filenames <- c()
-modes <- c()
-puritys <- c()
-ploidys <- c()
-dipLogRs <- c()
-
-getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
-
-#for(file in list.files(pattern='.gz$')) {
-
+  rcmat = readSnpMatrix(arg_in, perl.pileup = F)
+  #print(rcmat)
+  #stop()
+  filenames <- c()
+  modes <- c()
+  puritys <- c()
+  ploidys <- c()
+  dipLogRs <- c()
   purity.vals <- c()
   ploidy.vals <- c()
   dipLogR.vals <- c()
   cncf.vals <- list()
- # rcmat = readSnpMatrix(file, perl.pileup = F)
+
+  getmode <- function(v) {
+     uniqv <- unique(v)
+     uniqv[which.max(tabulate(match(v, uniqv)))]
+   }
+
 
   for(i in seq(10)) {
-        xx = preProcSample(rcmat)
-        oo=procSample(xx, cval=150)
-        fit=emcncf(oo, maxiter = 1000)
-        purity.vals[i] <- signif(fit[['purity']], 3)
-        ploidy.vals[i] <- fit[['ploidy']]
-        dipLogR.vals[i] <- oo[['dipLogR']]
-        cncf.vals[[i]] <- fit[['cncf']]
-        plotSample(x=oo, emfit=fit)
+      xx = preProcSample(rcmat)
+      oo=procSample(xx, cval=150)
+      fit=emcncf(oo, maxiter = 1000)
+      purity.vals[i] <- signif(fit[['purity']], 3)
+      ploidy.vals[i] <- fit[['ploidy']]
+      dipLogR.vals[i] <- oo[['dipLogR']]
+      cncf.vals[[i]] <- fit[['cncf']]
+      plotSample(x=oo, emfit=fit)
    }
 
    mode <- getmode(purity.vals)
@@ -50,10 +47,10 @@ getmode <- function(v) {
    dipLogRs <- c(dipLogRs, dipLogR)
 #}
 
-df <- data.frame(name=filenames, mode=modes, purity=puritys, ploidy=ploidys, dipLogR=dipLogRs)
-df.iter <- data.frame(name=filenames, purity=purity.vals, ploidy=ploidy.vals, dipLogR=dipLogR.vals)
-write.table(df,paste0(arg_out,arg_name,'.purity.txt'), quote=F, sep="\t", row.names=F)
-write.table(df.iter, paste0(arg_out,arg_name,'.purityfinal.txt') , quote=F, sep="\t", row.names=F)
+  df <- data.frame(name=filenames, mode=modes, purity=puritys, ploidy=ploidys, dipLogR=dipLogRs)
+  df.iter <- data.frame(name=filenames, purity=purity.vals, ploidy=ploidy.vals, dipLogR=dipLogR.vals)
+  write.table(df,paste0(arg_out,arg_name,'.optimalpurityvalue.txt'), quote=F, sep="\t", row.names=F)
+  write.table(df.iter, paste0(arg_out,arg_name,'.iterpurityvalues.txt') , quote=F, sep="\t", row.names=F)
 }
 
 args <- commandArgs( trailingOnly = TRUE )
