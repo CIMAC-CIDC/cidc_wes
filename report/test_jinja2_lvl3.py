0580a7dcfad4ee49910a834f2fd70dffdaa7400e
lentaing <lentaing@jimmy.harvard.edu>
2019-09-20 02:28:15 +0000

    WES_REPORT: ADDING wes_level3.html

---
+++ report/test_jinja2_lvl3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import jinja2
+
+TEMPLATE_FILE = "wes_level3.html"
+OUT_FILE = "output/wes_level3.html"
+
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+template = templateEnv.get_template(TEMPLATE_FILE)
+
+#HERE is where we populate a dictionarity of associated values
+nav_list = [('wes_level1.html','WES_Level1'),
+            ('wes_level2.html','WES_Level2'),
+            ('wes_level3.html','WES_Level3')]
+
+sidebar = [("clonality", "Clonality")]
+pg_name = 'WES_LEVEL_3'
+
+wes_report_vals = {'top_nav_list':nav_list, 'sidebar_nav': sidebar,
+                   'page_name': pg_name}
+    
+template.stream(wes_report_vals).dump(OUT_FILE)  
+
