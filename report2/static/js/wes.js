62d9f0f8dea8c01000a94bf8a8770982dcf88ebf
lentaing <lentaing@jimmy.harvard.edu>
2020-07-09 19:39:46 +0000

    NEW REPORT: Minor- removing drop down menu from navbar

--- report2/static/js/wes.js
+++ report2/static/js/wes.js
@@ -15,14 +15,3 @@ $('.wes-image-modal').on('click',function(){
     $('#wesImageModal').modal({show:true});
     $('#wesImageModal_img').attr("src", this.src);
 });
-
-//DELETE the following
-/* function to handle the dropdown select --not fully implemented */
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-    currVal = $("#menu-toggle").text();
-    nextVal = (currVal=='X') ? '>' : 'X';
-    $("#menu-toggle").text(nextVal);
-});
-
