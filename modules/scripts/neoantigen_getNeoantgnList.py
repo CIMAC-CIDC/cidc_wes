8fb6e031eb7effc66bfbde2f2f8c14285c871b81
lentaing <lentaing@jimmy.harvard.edu>
2021-08-04 04:42:12 +0000

    PVACTOOLS: Upgrading to pvactools 2.0.2 complete

--- modules/scripts/neoantigen_getNeoantgnList.py
+++ modules/scripts/neoantigen_getNeoantgnList.py
@@ -10,6 +10,7 @@ def main():
     usage = "USAGE: %prog -f [filtered.tsv] -o [output file.txt]"
     optparser = OptionParser(usage=usage)                    
     optparser.add_option("-f", "--file", help="neoantigen filter.tsv file")
+    optparser.add_option("-r", "--rna", help="include rna evidence (default: False)", action="store_true", default=False)
     optparser.add_option("-o", "--out", help="output file")
 
     (options, args) = optparser.parse_args(sys.argv)
@@ -20,9 +21,14 @@ def main():
         
     f = open(options.file)
     #Fields we want to pick out from the pvacseq filtered output file
-    fields = ['Gene Name', 'Mutation', 'Protein Position', 'HGVSc', 'HGVSp',
-              'HLA Allele', 'MT Epitope Seq', 'MT IC50', 'WT IC50',
-              'Fold Change', 'Tumor DNA VAF', 'Score']
+    fields = ['Gene Name', 'Mutation', 'Variant Type', 'HGVSc', 'HGVSp',
+              'HLA Allele', 'MT Epitope Seq', 
+              'Tumor DNA Depth', 'Tumor DNA VAF',
+              'Best MT Score', 'Corresponding WT Score', 'Corresponding Fold Change']
+    #ADD  'Tumor RNA Depth', 'Tumor RNA VAF'
+    if options.rna:
+        fields.insert(9, 'Tumor RNA Depth')
+        fields.insert(10, 'Tumor RNA VAF')
     hdr = f.readline().strip().split("\t")
     results = []
     for l in f:
@@ -34,7 +40,17 @@ def main():
 
     out = open(options.out, 'w')
     #print out the header
-    out.write("%s\n" % "\t".join(fields))
+    #SHORTEN the long fields names:
+    _map = {'MT Epitope Seq': 'Epitope Seq',
+            'Tumor DNA Depth': 'DNA Depth',
+            'Tumor DNA VAF': 'DNA VAF',
+            'Tumor RNA Depth': 'RNA Depth',
+            'Tumor RNA VAF': 'RNA VAF',
+            'Best MT Score': 'MT Score',
+            'Corresponding WT Score': 'WT Score',
+            'Corresponding Fold Change': 'Fold Change'}
+    new_fields = list(map(lambda x: _map[x] if x in _map else x, fields))
+    out.write("%s\n" % "\t".join(new_fields))
     for row in results:
         out.write("%s\n" % "\t".join(row))
     out.close()
