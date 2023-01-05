8d01f947f2b913139ba674b9deca88234f2568e9
Jacob Geisberg <jacobg@ds.dfci.harvard.edu>
2023-01-05 10:04:56 -0500

    DEVELOP: Fixed empty string sample issue in concat_config.py when handling nonpaired samples.

--- shell_scripts/concat_config.py
+++ shell_scripts/concat_config.py
@@ -30,19 +30,21 @@ def main():
         for l in f:
             (tmr, nrm) = l.strip().split(",")
             #Add tumor pairs
-            tmr_r1 = ["%s/%s/r1_L1.fastq.gz" % (path,tmr),
-                      "%s/%s/r1_L2.fastq.gz" % (path,tmr)]
-            tmr_r2 = ["%s/%s/r2_L1.fastq.gz" % (path,tmr),
-                      "%s/%s/r2_L2.fastq.gz" % (path,tmr)]
-            tmp['samples']['%s_R1' % tmr] = tmr_r1
-            tmp['samples']['%s_R2' % tmr] = tmr_r2
+            if tmr != '':
+                tmr_r1 = ["%s/%s/r1_L1.fastq.gz" % (path,tmr),
+                          "%s/%s/r1_L2.fastq.gz" % (path,tmr)]
+                tmr_r2 = ["%s/%s/r2_L1.fastq.gz" % (path,tmr),
+                          "%s/%s/r2_L2.fastq.gz" % (path,tmr)]
+                tmp['samples']['%s_R1' % tmr] = tmr_r1
+                tmp['samples']['%s_R2' % tmr] = tmr_r2
             #Add normal pairs
-            nrm_r1 = ["%s/%s/r1_L1.fastq.gz" % (path,nrm),
-                      "%s/%s/r1_L2.fastq.gz" % (path,nrm)]
-            nrm_r2 = ["%s/%s/r2_L1.fastq.gz" % (path,nrm),
-                      "%s/%s/r2_L2.fastq.gz" % (path,nrm)]
-            tmp['samples']['%s_R1' % nrm] = nrm_r1
-            tmp['samples']['%s_R2' % nrm] = nrm_r2
+            if nrm != '':
+                nrm_r1 = ["%s/%s/r1_L1.fastq.gz" % (path,nrm),
+                          "%s/%s/r1_L2.fastq.gz" % (path,nrm)]
+                nrm_r2 = ["%s/%s/r2_L1.fastq.gz" % (path,nrm),
+                          "%s/%s/r2_L2.fastq.gz" % (path,nrm)]
+                tmp['samples']['%s_R1' % nrm] = nrm_r1
+                tmp['samples']['%s_R2' % nrm] = nrm_r2
     
     yaml_out = open(options.output, "w")
     yaml.dump(tmp, yaml_out) 
